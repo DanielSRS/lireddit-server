f76c2dafbcfb71396707f244977656085c59dfae
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2022-01-08 23:30:52 +0000

    :sparkles: :heavy_plus_sign: Setup graphql and apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
+import express from 'express';
 import { __prod__ } from './constants';
 //import { Post } from './entities/Post';
 import mikroOrmConfig from './mikro-orm.config';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloRevolver } from './resolvers/hello';
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);  // conecta com o banco de dados
@@ -13,6 +17,23 @@ const main = async () => {
 
     //const posts = await orm.em.find(Post, {});
     //console.log(posts);
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloRevolver],
+            validate: false,
+        })
+    });
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000');
+    });
 }
 
 main ().catch((error) => {
