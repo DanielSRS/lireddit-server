1e09aeb1087f07c4cb68158e969a17d19230259b
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2022-01-08 22:36:07 +0000

    :card_file_box: :wrench: :heavy_plus_sign: Setup Mikro-orm
    configure database connection;
    configure migrations;
    configure entities;

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const __prod__ = process.env.NODE_ENV === 'production';  // esta executando em produção ou não
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+//import { Post } from './entities/Post';
+import mikroOrmConfig from './mikro-orm.config';
+
+const main = async () => {
+    const orm = await MikroORM.init(mikroOrmConfig);  // conecta com o banco de dados
+    await orm.getMigrator().up();  // roda as migrations
+
+    // executa o sql
+    //const post = orm.em.create(Post, {title: 'my first post'});
+    //await orm.em.persistAndFlush(post);
+
+    //const posts = await orm.em.find(Post, {});
+    //console.log(posts);
+}
+
+main ().catch((error) => {
+    console.error(error);
+});
